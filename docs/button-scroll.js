30fc0154208df9947a80b7756bb2e6513ad47055
gbmedeiros00 <157700662+gbmedeiros00@users.noreply.github.com>
2024-10-10 10:52:54 -0300

    Atualizado portfolio

--- docs/button-scroll.js
+++ docs/button-scroll.js
@@ -1,15 +1,8 @@
-let lastScrollTop = 0; // Armazena a posição do scroll anterior
-const header = document.querySelector('.topo'); // Seleciona o header
-
-window.addEventListener('scroll', function () {
-    let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
-
-    if (currentScroll > lastScrollTop) {
-        // Quando o usuário está rolando para baixo
-        header.style.top = '-58%'; // Esconde o header (ajuste conforme a altura do header)
+window.onscroll = function() {
+    const botao = document.querySelector('.topo button');
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        botao.style.display = "block"; // Mostrar o botão
     } else {
-        // Quando o usuário está rolando para cima
-        header.style.top = '0';
+        botao.style.display = "none"; // Esconder o botão
     }
-    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // Impede valores negativos no topo
-});
+};
