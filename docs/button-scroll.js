5cea6fb0fb12b4e6d6343c2a0291e4eb68bdc2be
Gabriel Medeiros <157700662+gbmedeiros00@users.noreply.github.com>
2025-06-22 15:38:26 -0300

    Adicionado projeto 2° semestre e alguns ajustes de design

--- docs/button-scroll.js
+++
@@ -1,8 +0,0 @@
-window.onscroll = function() {
-    const botao = document.querySelector('.topo button');
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        botao.style.display = "block"; // Mostrar o botão
-    } else {
-        botao.style.display = "none"; // Esconder o botão
-    }
-};
